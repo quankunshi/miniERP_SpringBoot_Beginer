3ba7164918f69eb2aa5aa8e17f84a7caeca58c1a
Nguyễn Minh Quân <99945660+quankunshi@users.noreply.github.com>
2023-05-26 14:05:47 +0700

    User ModelMapper
    Replace getter setter with map conditionally

--- src/main/java/project/webapplication/erpsystem/service/imple/EmployeeServiceImpl.java
+++ src/main/java/project/webapplication/erpsystem/service/imple/EmployeeServiceImpl.java
@@ -1,7 +1,9 @@
 package project.webapplication.erpsystem.service.imple;
 
+import org.modelmapper.Condition;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
+import org.modelmapper.spi.MappingContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import project.webapplication.erpsystem.dto.EmployeeDto;
@@ -14,15 +16,18 @@ import project.webapplication.erpsystem.service.EmployeeService;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.Scanner;
+import java.util.stream.Collectors;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
     @Autowired
     private EmployeesRepository employeesRepository;
     @Autowired
-    private  SalaryRepository salaryRepository;
+    private SalaryRepository salaryRepository;
 
     private ModelMapper modelMapper = new ModelMapper();
+
 
     public EmployeeServiceImpl() {
     }
@@ -33,46 +38,61 @@ public class EmployeeServiceImpl implements EmployeeService {
         this.modelMapper = modelMapper;
     }
 
-    public EmployeeServiceImpl(EmployeesRepository employeesRepository, SalaryRepository salaryRepository) {
-        this.employeesRepository = employeesRepository;
-        this.salaryRepository = salaryRepository;
-    }
 
     @Override
     public List<EmployeeDto> findAll() {
         List<Employees> employeeList = employeesRepository.findAll();
-        TypeToken<List<EmployeeDto>> typeToken = new TypeToken<List<EmployeeDto>>(){};
-        return modelMapper.map(employeeList, typeToken.getType());
+        return employeeList.stream()
+                .map(employee -> modelMapper.map(employee, EmployeeDto.class))
+                .collect(Collectors.toList());
     }
 
     @Override
     public EmployeeDto findById(String id) {
         Employees employees = employeesRepository.findById(id).orElse(null);
-        if (employees!= null){
-            return modelMapper.map(employees,EmployeeDto.class);
+        if (employees != null) {
+            return modelMapper.map(employees, EmployeeDto.class);
         }
         return null;
     }
 
     @Override
     public void save(EmployeeDto employeeDto) {
-         Employees employees = modelMapper.map(employeeDto, Employees.class);
-         employeesRepository.save(employees);
+        Employees employees = modelMapper.map(employeeDto, Employees.class);
+        employeesRepository.save(employees);
     }
 
     @Override
     public boolean existsById(String id) {
-       return employeesRepository.existsById(id);
+        return employeesRepository.existsById(id);
     }
 
     @Override
     public void update(EmployeeDto employeeDto) {
+        Condition<Object, Object> notNullCondition = new Condition<Object, Object>() {
+            /**
+             *Is used to check if a mapping condition should be applied to a particular mapping context.
+             *@return a boolean value that determines whether the mapping condition should be applied.
+             * */
+            @Override
+            public boolean applies(MappingContext<Object, Object> context) {
+                //this is its condition, since the attributes are both integer(!null) and String(isEmpty)
+                //if only numbers then !null or only String then isEmpty
+                return context.getSource() != null && !context.getSource().toString().isEmpty();
+            }
+        };
+        modelMapper.getConfiguration().setPropertyCondition(notNullCondition);
+
         Employees employeesUpdate = employeesRepository.findById(employeeDto.getEmployeeId()).orElse(null);
-        if (employeesUpdate!=null) {
+
+        if (employeesUpdate != null) {
+            modelMapper.map(employeeDto, employeesUpdate);
+            employeesRepository.save(employeesUpdate);
+            /* this way is also ok
             if (!employeeDto.getEmployeeName().isEmpty()) {
                 employeesUpdate.setEmployeeName(employeeDto.getEmployeeName());
             }
-            if (employeeDto.getEmployeeAge()!=null) {
+            if (employeeDto.getEmployeeAge() != null) {
                 employeesUpdate.setEmployeeAge(employeeDto.getEmployeeAge());
             }
             if (!employeeDto.getEmployeeNumberPhone().isEmpty()) {
@@ -81,14 +101,17 @@ public class EmployeeServiceImpl implements EmployeeService {
             if (!employeeDto.getEmployeeAddress().isEmpty()) {
                 employeesUpdate.setEmployeeAddress(employeeDto.getEmployeeAddress());
             }
-            employeesRepository.save(employeesUpdate);
+            */
+
         }
+
+
     }
 
     @Override
-    public void deleteById(String  id){
+    public void deleteById(String id) {
         Salary salary = salaryRepository.findByEmployee(employeesRepository.findById(id).get());
-        if (salary!=null) {
+        if (salary != null) {
             salaryRepository.delete(salary);
         }
         // Xóa nhân viên
